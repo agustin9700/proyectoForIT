75b33a311ed9e832207ed927a4356f10fa242c98
agustin9700 <ryuk10xd@gmail.com>
2025-03-23 22:30:02 -0300

    cors

--- backend-project/index.js
+++ backend-project/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const cors = require('cors')
 const taskRoutes = require("./src/routes/task.routes");
 
 app.use(express.json());
+app.use(cors())
 app.use("/api/tasks", taskRoutes);
 
 
